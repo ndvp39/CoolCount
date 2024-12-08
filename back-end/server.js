6ae3a6f987196b37f07978e66d468db1e8312f43
ndvp39 <ndvp39@gmail.com>
2024-12-08 18:50:27 +0200

    deploy-vercel

--- back-end/server.js
+++ back-end/server.js
@@ -2,9 +2,21 @@ const express = require('express'); // Import the Express framework
 const cors = require('cors'); // Middleware for handling CORS
 const { initializeApp, cert } = require('firebase-admin/app'); // Firebase Admin SDK initialization
 const { getFirestore } = require('firebase-admin/firestore'); // Firestore database service
-const serviceAccount = require('./firebase-key.json'); // Firebase service account key
 const SerialPort = require('serialport'); // Serial port for Arduino communication
 const nodemailer = require('nodemailer'); // For sending emails
+
+const serviceAccount = {
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL
+  };
 
 // Initialize the Express app
 const app = express();
